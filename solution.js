626e16045624ac1570a935437ff984673b7afe58
Md. Mahmodul Hassen Khan Shadin <shadhinkhan28@gmail.com>
2025-08-13 15:47:51 +0000

    update task 03

--- solution.js
+++ solution.js
@@ -33,3 +33,31 @@ if(typeof str !== "string" || str === Boolean ){
  }
 }
 
+// Problem 03
+
+function bestTeam( player1, player2 ) {
+// You have to write your code here
+
+if(typeof player1 !== "object" && player2 !== 'object' || player1 !== "string" && player2 !== 'string'){
+
+    let alertSms = "Invalid" ;
+    return alertSms ;
+    
+ } 
+ else {
+     let player1Score = player1.foul + player1.cardY + player1.cardR ;
+     let player2Score = player2.foul + player2.cardY + player2.cardR ;
+
+     if(player1Score > player2Score){
+
+        return player1.name ;
+     }
+     else if(player1Score === player2Score){
+            return "Tie" ;
+     }
+     else {
+        return player2.name ;
+     }
+      
+ }
+}
