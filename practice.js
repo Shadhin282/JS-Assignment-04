7ac553f394db196732ba330e22386aba6bfdcca1
Md. Mahmodul Hassen Khan Shadin <shadhinkhan28@gmail.com>
2025-08-13 14:27:27 +0000

    update first problem solution

--- practice.js
+++ practice.js
@@ -1,6 +1,17 @@
 function totalFine( fare ) {
 // You have to write your code here
- if(typeof fare){
+ if(typeof fare !== "number" || fare <=0 ){
+
+    let alertSms = "Invalid" ;
+    return alertSms ;
     
+ } 
+ else {
+    let farePercentage = fare * (20/100) ;
+    const totalFine = fare + farePercentage  + 30;
+    return totalFine ;
  }
 }
+
+let fare = 0;
+console.log(totalFine(fare))
---
+++ solution.js
@@ -0,0 +1,15 @@
+
+function totalFine( fare ) {
+// You have to write your code here
+ if(typeof fare !== "number" || fare <=0 ){
+
+    let alertSms = "Invalid" ;
+    return alertSms ;
+    
+ } 
+ else {
+    let farePercentage = fare * (20/100) ;
+    const totalFine = fare + farePercentage  + 30;
+    return totalFine ;
+ }
+}
